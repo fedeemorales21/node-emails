56804ca8d8d2c2c2fbee7ba1a7528ba0262a6d3c
Federico Morales <fedeemorales21@gmail.com>
2020-09-18 20:18:14 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path')
+const { join } = require('path')
 
 const app = express()
 
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(join(__dirname,'public')))
 app.use(require('./routes/index'))
 
 
